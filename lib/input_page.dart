e858bb2498ee1f628d98354541371a5e1b7eb4f7
Christopher Tineo <christophertineo02@gmail.com>
2023-06-09 07:49:10 -0400

    chore: :tada: initialize project

---
+++ lib/input_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<InputPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          'Hello User',
+        ),
+      ),
+    );
+  }
+}
